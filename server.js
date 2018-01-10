a24ddd0f8f27ee48549f06db0ec23bd910efb401
人間失格 <jacob@dnamicro.com>
2018-01-10 15:52:10 +0800

    revert server.js

--- server.js
+++ server.js
@@ -1,33 +1,16 @@
-// const { default: Server } = require('./src/Server/http')
-// const { default: routes } = require('./app/route')
-// const { env: init_env, Config, base_path } = require('./src/utility')
-// const env = init_env()
-// const config = new Config(env, base_path() + '/config')
+const { default: Server } = require('./src/Server/http')
+const { default: routes } = require('./app/route')
+const { env: init_env, Config, base_path } = require('./src/utility')
+const env = init_env()
+const config = new Config(env, base_path() + '/config')
 
-// const args = Object.assign(
-//   {},
-//   config.get('app'),
-//   { publicPath: base_path() + '/public' }
-// )
+const args = Object.assign(
+  {},
+  config.get('app'),
+  { publicPath: base_path() + '/public' }
+)
 
-// const app = Server(args)
+const app = Server(args)
 
-// app.configure(app => routes(app))
-// app.start()
-
-const { default: db } = require('./src/Database/rethink')
-
-const X = new db({
-  host: '172.17.0.2'
-})
-
-X.open()
-X.changeDb('test')
-
-const res = X.console((r) => {
-  return r.table('users')
-})
-
-res.then(r => {
-  console.log(r)
-})
+app.configure(app => routes(app))
+app.start()

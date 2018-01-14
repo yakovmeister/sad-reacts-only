0d299a5b66e822b39d51e795977998a43942c4c2
yakovmeister <electro7bug@gmail.com>
2018-01-14 19:57:30 +0800

    revert server.js

--- server.js
+++ server.js
@@ -1,26 +1,18 @@
-// const { default: Server } = require('./src/Server/http')
-// const { default: routes } = require('./app/route')
-// const { env: init_env, Config, base_path } = require('./src/utility')
-// const env = init_env()
-// const config = new Config(env, base_path() + '/config')
+const { default: Server } = require('./src/Server/http')
+const { default: routes } = require('./app/route')
+const { env: init_env, Config, base_path } = require('./src/utility')
+const env = init_env()
+const config = new Config(env, base_path() + '/config')
 const { default: Thoughts } = require('./src/Database/Thoughts')
-// global.thoughts = new Thoughts()
+global.thoughts = new Thoughts()
 
-// const args = Object.assign(
-//   {},
-//   config.get('app'),
-//   { publicPath: base_path() + '/public' }
-// )
+const args = Object.assign(
+  {},
+  config.get('app'),
+  { publicPath: base_path() + '/public' }
+)
 
-// const app = Server(args)
+const app = Server(args)
 
-// app.configure(app => routes(app))
-// app.start()
-const t = new Thoughts()
-
-const Users = t.createModel('users')
-
-const users = new Users()
-
-
-users.all().then(console.log)
+app.configure(app => routes(app))
+app.start()
